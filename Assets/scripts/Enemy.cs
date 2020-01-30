9e96aa2535cf3a9776f1a1dd0a7b40e5995f1b4b
Vince van Doorn <323888>
2020-01-30 23:01:39 +0100

    t/m 4.4

---
+++ Assets/scripts/Enemy.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    // Start is called before the first frame update
+    Rigidbody enemyRb;
+    GameObject player;
+    public float speed;
+    void Start()
+    {
+        enemyRb = GetComponent<Rigidbody>();
+        player = GameObject.Find("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //zorgt ervoor dat de enemy achter het gameobject player aangaat
+        Vector3 lookDirection = (player.transform.position - transform.position).normalized;
+        enemyRb.AddForce(lookDirection * speed);
+    }
+}
---
+++ Assets/scripts/PlayerController.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    private Rigidbody playerRb;
+    private GameObject focalPoint;
+
+    public bool hasPowerup = false;
+    
+    //de powerupstrength (kracht groter dan standaard)
+    private float powerupStrength = 15.0f;
+
+    public GameObject powerupIndicator;
+    
+    
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+        focalPoint = GameObject.Find("Focal Point");
+
+    }
+
+
+    void Update()
+    {
+        float forwardInput = Input.GetAxis("Vertical");
+        playerRb.AddForce(focalPoint.transform.forward * speed * forwardInput);
+        
+        powerupIndicator.transform.position = transform.position + new Vector3(0, -0.5f, 0);
+    }
+    
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        // als je tegen een powerup botst verandert de boolean naar true en het vernietigt de andere gameobjecten
+        if (other.CompareTag("Powerup"))
+        {
+            hasPowerup = true;
+            Destroy(other.gameObject);
+            StartCoroutine(PowerupCountdownRoutine());
+            powerupIndicator.gameObject.SetActive(true);
+        }
+    }
+
+    IEnumerator PowerupCountdownRoutine()
+    {
+        yield return new WaitForSeconds(7);
+        hasPowerup = false;
+        powerupIndicator.gameObject.SetActive(false);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("enemy") && hasPowerup)
+        {
+            Rigidbody enemyRigidbody = collision.gameObject.GetComponent<Rigidbody>();
+            Vector3 awayFromPlayer = (collision.gameObject.transform.position - transform.position);
+            
+            Debug.Log("Player collided with " + collision.gameObject + " with powerup set to " + hasPowerup);
+            
+            //geeft je extra "stuiter"kracht wanneer je een powerup hebt
+            enemyRigidbody.AddForce(awayFromPlayer * powerupStrength, ForceMode.Impulse);
+        }
+    }
+}
